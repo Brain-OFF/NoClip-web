a0a9e3d615da4ec605de2ae9a2594566f715e001
xDrais <yassin001009@gmail.com>
2022-03-01 11:34:36 +0100

    jason crud coach

--- src/Controller/MobilecoachreservationController.php
+++ src/Controller/MobilecoachreservationController.php
@@ -43,7 +43,7 @@ class MobilecoachreservationController extends AbstractController
 
     public function addcoach(Request $request, NormalizerInterface $normalizer)
     {
-        $user= $this->getDoctrine()->getManager();
+
         $coach = new Coach();
         $coach->setName($request->get("name"));
         $coach->setLastname($request->get("lastname"));
@@ -57,5 +57,43 @@ class MobilecoachreservationController extends AbstractController
         $jsonContent = $normalizer->normalize($coach,'json',['groups'=>'coach']);
         return new Response(json_encode($jsonContent));
     }
+
+
+
+    /**
+     * @Route("/updatecoachmobile/{id}",name="updatecoachmobile")
+     */
+    public function update(Request $request,$id,NormalizerInterface $normalizer){
+        $coach= $this->getDoctrine()->getRepository(Coach::class)->find($id);
+        $coach->setName($request->get("name"));
+        $coach->setLastname($request->get("lastname"));
+        $coach->setRank($request->get("rank"));
+        $coach->setCategorie($request->get("categorie"));
+
+        $em = $this->getDoctrine()->getManager();
+        $em->flush();
+        $jsonContent = $normalizer->normalize($coach,'json',['groups'=>'coach']);
+
+        return new Response(json_encode($jsonContent));
+
+    }
+    /**
+     * @Route("/deletecoachmobile/{id}", name="deletecoachmobile")
+     */
+    public function delcoach(Request $request,NormalizerInterface $normalizer,$id):Response
+    {
+
+
+        $em= $this->getDoctrine()->getManager();
+        $coach=$em->getRepository(Coach::class)->find($id);
+        $em->remove($coach);
+        $em->flush();
+        $jsonContent = $normalizer->normalize($coach,'json',['groups'=>'coach']);
+        return new Response("User deleted Successfully ".json_encode($jsonContent));
+    }
+
+
+
+
 
 }
